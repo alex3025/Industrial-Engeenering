79dd8aa60916efebee05192e8abd931e1cd206f2
LolzDEV <lollotorres06@gmail.com>
2020-08-28 20:35:31 +0200

    Sistemata distillation tower

--- com/indeng/entity/TileEntityDistillationTank.java
+++ com/indeng/entity/TileEntityDistillationTank.java
@@ -1,5 +1,8 @@
 package com.indeng.entity;
 
+import com.indeng.api.DistillationTowerFuel.FuelType;
+
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.ForgeDirection;
 import net.minecraftforge.liquids.ILiquidTank;
@@ -14,8 +17,45 @@ public class TileEntityDistillationTank extends TileEntity implements ITankConta
 	ForgeDirection currentDirection;
 	
 	
+	@Override
+	public void writeToNBT(NBTTagCompound par1NBTTagCompound)
+    {
+        super.writeToNBT(par1NBTTagCompound);
+
+        if(currentDirection == ForgeDirection.NORTH) {
+        	par1NBTTagCompound.setShort("Dir", (short)0);
+        } else if(currentDirection == ForgeDirection.EAST) {
+        	par1NBTTagCompound.setShort("Dir", (short)1);
+        } else if(currentDirection == ForgeDirection.SOUTH) {
+        	par1NBTTagCompound.setShort("Dir", (short)2);
+        } else if(currentDirection == ForgeDirection.WEST) {
+        	par1NBTTagCompound.setShort("Dir", (short)3);
+        } 
+        
+    }
+	
+	@Override
+	public void readFromNBT(NBTTagCompound par1NBTTagCompound)
+    {
+        super.readFromNBT(par1NBTTagCompound);
+
+        if(par1NBTTagCompound.getShort("Dir") == (short)0) {
+        	currentDirection = ForgeDirection.NORTH;
+        } else if(par1NBTTagCompound.getShort("Dir") == (short)1) {
+        	currentDirection = ForgeDirection.EAST;
+        } else if(par1NBTTagCompound.getShort("Dir") == (short)2) {
+        	currentDirection = ForgeDirection.SOUTH;
+        } else if(par1NBTTagCompound.getShort("Dir") == (short)3) {
+        	currentDirection = ForgeDirection.WEST;
+        }
+    }
+	
 	public TileEntityDistillationTank(ForgeDirection meta) {
 		this.currentDirection = meta;
+	}
+	
+	public TileEntityDistillationTank() {
+		this.currentDirection = ForgeDirection.EAST;
 	}
 	
 	@Override
